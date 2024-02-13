1c27ce7df3766b04f527e48830d803b6c311d8de
RAC22 <robrac83@gmail.com>
2024-02-13 00:10:27 -0800

    remove false log

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let searchForClaimButton = setInterval(pushClaim, 750)
 function pushClaim() {
 	const claimButton = document.querySelector('[aria-label="Claim Bonus"]')
 	if (claimButton) {
-		claimButton.click()
 		console.log(`Claimed bonus!`)
+		claimButton.click()
 	}
 }
